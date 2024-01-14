1f29384b8cacbf176dec5b2971f3128b156fae72
pgxtips <brandongill123@gmail.com>
2024-01-14 02:38:05 +0000

    Added new feature: retrieval of origin link of article

--- src/news_client/defs.rs
+++ src/news_client/defs.rs
@@ -3,8 +3,10 @@ use serde::Serialize;
 #[derive(Serialize)]
 pub struct News {
     pub title: String,
-    pub link: String,
+    pub rss_link: String,
+    pub origin_link: String,
     pub description: String,
+    pub source: String,
     pub pub_date: String,
     pub guid: String,
     pub comments: String,
@@ -13,8 +15,10 @@ impl News {
     pub fn new() -> News {
         News {
             title: String::new(),
-            link: String::new(),
+            rss_link: String::new(),
+            origin_link: String::new(),
             description: String::new(),
+            source: String::new(),
             pub_date: String::new(),
             guid: String::new(),
             comments: String::new()
--- src/news_client/mod.rs
+++ src/news_client/mod.rs
@@ -40,15 +40,49 @@ async fn get_search_rss(query: &str) -> Result<Channel, Box<dyn Error>>{
     Ok(channel)
 }
 
-fn process_news_channel(channel: Result<Channel, Box<dyn Error>>) -> Result<Vec<News>, Box<dyn Error>>{
+async fn get_link(rss_link: String, news_source: String) -> String {
+    let resp = reqwest::get(&rss_link).await;
 
+    match resp {
+        Ok(resp) => {
+            let resp_text = resp.text().await;
+
+            match resp_text {
+                Ok(resp_text) => {
+                    let start_index = resp_text.find(&news_source).unwrap_or_default();
+                    let sub_str = &resp_text[start_index..];
+                    let end_index = sub_str.find("\"").unwrap_or_default();
+                    let link = &sub_str[..end_index];
+                    let link = link.replace("\"", "");
+                    link
+                },
+                Err(e) => {
+                    log::error!("Error: {}", e);
+                    String::new()
+                }
+            }
+        }, 
+        Err(e) => {
+            log::error!("Error: {}", e);
+            String::new()
+        }
+    }
+}
+
+async fn process_news_channel(channel: Result<Channel, Box<dyn Error>>) -> Result<Vec<News>, Box<dyn Error>>{
     let feed = channel?;
     let mut news: Vec<News> = Vec::new();
 
     for item in feed.items(){
         let mut new_item: News = News::new();
         new_item.title = item.title().unwrap_or_default().to_string();
-        new_item.link = item.link().unwrap_or_default().to_string();
+        new_item.source = item
+            .source()
+            .unwrap_or(&rss::Source::default())
+            .url()
+            .to_string();
+        new_item.rss_link = item.link().unwrap_or_default().to_string();
+        new_item.origin_link = get_link(new_item.rss_link.to_owned(), new_item.source.to_owned()).await;
         new_item.description = item.description().unwrap_or_default().to_string();
         new_item.pub_date = item.pub_date().unwrap_or_default().to_string();
         new_item.guid = item.guid().unwrap_or(&rss::Guid::default()).value().to_string();
@@ -63,12 +97,18 @@ fn process_news_channel(channel: Result<Channel, Box<dyn Error>>) -> Result<Vec<
 #[allow(non_snake_case)]
 pub mod NewsClient {
     use super::*;
+    use std::time::{Duration, Instant};
     pub use crate::news_client::constants::Topic as Topic;
     pub use crate::news_client::defs::News as News;
 
     pub async fn get_top_stories() -> Vec<News>{
+        let time_start = Instant::now();
+
         let rss = get_top_stories_rss().await;
-        let news = process_news_channel(rss);
+        let news = process_news_channel(rss).await;
+
+        let time_ellapsed = Instant::now().duration_since(time_start);
+        log::info!("Time ellapsed: {:?}", time_ellapsed);
 
         match news {
             Ok(news) => news,
@@ -80,8 +120,13 @@ pub mod NewsClient {
     }
 
     pub async fn get_topic(topic: Topic) -> Vec<News>{
+        let time_start = Instant::now();
+
         let rss = get_topic_rss(topic).await;
-        let news = process_news_channel(rss);
+        let news = process_news_channel(rss).await;
+
+        let time_ellapsed = Instant::now().duration_since(time_start);
+        log::info!("Time ellapsed: {:?}", time_ellapsed);
 
         match news {
             Ok(news) => news,
@@ -93,8 +138,13 @@ pub mod NewsClient {
     }
 
     pub async fn get_search(query: &str) -> Vec<News>{
+        let time_start = Instant::now();
+
         let rss = get_search_rss(query).await;
-        let news = process_news_channel(rss);
+        let news = process_news_channel(rss).await;
+
+        let time_ellapsed = Instant::now().duration_since(time_start);
+        log::info!("Time ellapsed: {:?}", time_ellapsed);
 
         match news {
             Ok(news) => news,
